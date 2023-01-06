bc131edbb2c59e9a04e0c423bab77e3f95c8679a
Koltunov <Sergey.Koltunov@pepsico.com>
2023-01-06 21:50:23 +0300

    DATABASES changed to default again

--- craigav/settings.py
+++ craigav/settings.py
@@ -88,19 +88,19 @@ WSGI_APPLICATION = "craigav.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 # default and worked in development
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
 DATABASES = {
-    'default': dj_database_url.config(
-        default='django.db.backends.sqlite3',
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default='django.db.backends.sqlite3',
+#         conn_max_age=600,
+#         conn_health_checks=True,
+#     )
+# }
 
 
 # Password validation
