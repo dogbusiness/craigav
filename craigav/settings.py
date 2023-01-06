f27ebaf429b26f06ec48a33ede7f50ddcf21234d
Koltunov <Sergey.Koltunov@pepsico.com>
2023-01-06 20:14:50 +0300

    Add debug=false, requirements.txt

--- craigav/settings.py
+++ craigav/settings.py
@@ -24,9 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-mkxcq@=h)8@p^44j1c_(5!w)%&u!-#i+3d_f=5*g@4s_zhb30^"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # FOR PHOTOS IN POSTS
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media/')
