057e3682de3a57030cb02c02fe27f41770767a7d
Koltunov <Sergey.Koltunov@pepsico.com>
2023-01-06 21:40:43 +0300

    pip install in build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 # exit on error
 set -o errexit
 
+pip install -r requirements.txt
 poetry install
 
 python manage.py collectstatic --no-input
+python manage.py makemigrations
 python manage.py migrate
