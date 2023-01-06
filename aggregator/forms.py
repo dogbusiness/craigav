637d8780d3c66de97a8eb1a75301464527976920
Koltunov <Sergey.Koltunov@pepsico.com>
2023-01-06 16:19:06 +0300

    PostForms min_value for price

--- aggregator/forms.py
+++ aggregator/forms.py
@@ -10,7 +10,7 @@ class PostForm(forms.Form):
     title = forms.CharField(max_length=50)
     category = forms.ChoiceField(label='Category', choices=CATEGORIES)
     subcategory = forms.ChoiceField(label='SubCategory', choices=SUBCATEGORIES)
-    price = forms.DecimalField(max_digits=12, decimal_places=2)
+    price = forms.DecimalField(max_digits=12, decimal_places=2, min_value=0.01)
     city = forms.CharField(max_length=200)
     description = forms.CharField(widget=forms.Textarea, max_length=1500)
     photo = forms.ImageField(required=False)
@@ -25,7 +25,7 @@ class EditPostForm(forms.Form):
     title = forms.CharField(max_length=50)
     category = forms.ChoiceField(label='Category', choices=CATEGORIES)
     subcategory = forms.ChoiceField(label='SubCategory', choices=SUBCATEGORIES)
-    price = forms.DecimalField(max_digits=12, decimal_places=2)
+    price = forms.DecimalField(max_digits=12, decimal_places=2, min_value=0.01)
     city = forms.CharField(max_length=200)
     description = forms.CharField(widget=forms.Textarea, max_length=1500)
 
